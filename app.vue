5f878ec8d673cc90415b8d39099ac48ccea5cdb9
Florent <florentheyraud.pro@gmail.com>
2023-04-02 16:33:32 +0200

    add html element and style

--- app.vue
+++ app.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>
-    <h1 class="font-bold text-2xl">
-      The <span class="text-4xl text-[#B2A4FF]">374th</span> todo list of my
-      life
-    </h1>
+  <div class="w-full min-h-screen bg-gray-100">
+    <section class="text-center pt-7">
+      <h1 class="text-4xl text-gray-800">
+        What are we doing <span class="text-[#B2A4FF]">today?</span>
+      </h1>
+    </section>
+    <section
+      class="w-11/12 max-w-full mx-auto flex items-center justify-center mt-7 bg-white shadow p-5 rounded"
+    >
+      <form @submit.prevent>
+        <input
+          type="text"
+          class="py-2 px-4 border border-blue-200 focus:outline-blue-300 rounded"
+          placeholder="Add a todo"
+        />
+        <button
+          class="border border-blue-200 py-2 px-4 ml-3 hover:bg-blue-200 transition-all duration-200 rounded"
+        >
+          Add Todo
+        </button>
+      </form>
+    </section>
   </div>
 </template>
